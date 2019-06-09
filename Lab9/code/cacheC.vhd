51a0c158bd83542f195c14f49b829b38fa230e49
unknown <cher9147@kettering.edu>
2019-06-09 19:30:30 -0400

    Finished CacheM.vhd some inputs and outputs and code added to CacheC.vhd

--- Lab9/code/cacheC.vhd
+++ Lab9/code/cacheC.vhd
@@ -4,14 +4,56 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity cacheC is
-    port (instruction : in bit_vector(31 downto 0);
-    tag : in bit_vector(9 downto 0));
+    port (clk, validIn, writeEnable:  in bit;
+    	tagIn: in bit_vector(9 downto 0);
+        instrIn, imDataIn: in bit_vector(31 downto 0);
+        
+     cacheOut, pcEnableOut, validOut: out bit;
+     indexOut: out bit_vector(3 downto 0);
+     tagOut: out bit_vector(9 downto 0);             
+    instruOut, imData : out bit_vector(31 downto 0));
 end cacheC;
 
 architecture behave of cacheC is
+
+signal tagCompare: bit;
+signal hit bit;
+signal timer : std_logic_unsigned(0 to 4):= "00000";  
+signal dat: bit_vector(31 downto 0);
+
+
 begin  -- behave
-    
-    
+imData <= instrIn;    
+indexOut <= instrIn(5 downto 2);
+tagOut <= instrIn(15 downto 6);
+
+tagCompare <= '1' when tagIn = instrIn(15 downto 6);
+hit <= tagCompare and validIn;
+
+instrucOut <= imDataIn when hit = '1' else
+	             "00000000000000000000000000000000";
+
+pcEnableOut <= '1' when hit = '1' else
+	                    '0';
+process(clk)
+	begin
+	if clk = '1' and hit '1' then
+           cacheOut <='0';
+           counter <= '00000';
+           validOut = '0';
+           end if;
+
+    if clk = '1' and hit '0' then
+            timer <= timer + 1;
+            cacheOut <= '0';
+            validOut <= '0';
+            end if;
+    if timer = "10100" then
+           cacheOut = '1';
+           counter <= "00000";
+           validOut <= '1';
+           end if;
+end process;
 
 end behave;
 
--- Lab9/code/cacheM.vhd
+++ Lab9/code/cacheM.vhd
@@ -3,33 +3,54 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity imem is
+entity cacheM is
     
-    port (index : in bit_vector(3 downto 0);
-        Tag : 
+    port (
+          indexIn : in bit_vector(3 downto 0);
+          tagIn : in bit_vector(9 downto 0);
+          clk,WE: in bit;
+          InstIn: bit_vector(31 downto 0);
 
 
-        Instruction : out bit_vector(31 downto 0);
-        Tag : out bit_vector (9 downto 0);
-        Valid : out bit);
+          InstrOut: out bit_vector(31 downto 0);
+          tagOut: out bit_vector(9 downto 0);
+          validOut: out bit);  
 
-end imem;
+end cacheM;
 
-architecture behave of imem is
-
+architecture behave of cacheM is
+    
     signal intAddress : natural;  -- unsigned integer address
     -- Type for memory (only 256+3 bytes implemented)
     --  (extra bytes needed in case of word accesses to last 3 bytes)
-    type memoryType is array (0 to 258) of bit_vector(7 downto 0);
+    type memoryType is array (0 to 15) of bit_vector(42 downto 0);
     -- Memory itself
     signal memory : memoryType;
+    signal data: bit_vector(42 downto 0);
+    alias valid: bit is data(42);
+    alias tag: bit_vector(9 downto 0) is data(41 downto 32);
+    alias Instruction: bit_vector(31 downto 0) is data(31 downto 0);
 
-begin  -- array
-    intAddress <= conv_integer(to_stdlogicvector(ReadAddress(7 downto 0)));
-    Instruction <=
-    memory(intAddress) &
-    memory(intAddress+1) &
-    memory(intAddress+2) &
-    memory(intAddress+3)
-    after 5 ns;
+begin  -- behave
+    
+
+     -- convert address to integer
+    intAddress <= conv_integer(to_stdlogicvector(indexIn));
+   
+    data <= memory(intAddress);
+
+    validOut <= valid;
+    tagOut<= tag;
+    InstrOut <= Instruction;      
+
+
+    process (clk)
+    begin
+        -- if rising edge of clock and WE, then write locations
+        if clk = '1' and WE = '1' then
+        memory(intAddress)  <= '1' & tagIn & InstIn;              
+
+        end if;
+    end process;
+        
 end behave;
