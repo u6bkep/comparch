26f4377c1278b0a868bf95f4784db7bc052babdf
u6bkep <u6bkep@gmail.com>
2019-06-09 19:33:49 -0400

    moving twards a proper cache implementation

--- Lab9/code/cacheM.vhd
+++ Lab9/code/cacheM.vhd
@@ -6,11 +6,12 @@ use ieee.std_logic_unsigned.all;
 entity imem is
     
     port (index : in bit_vector(3 downto 0);
-        Tag : 
+        TagIn : in bit_vector(9 downto 0);
+        WE : in bit;
+        InstIn : bit_vector(31 downto 0);
 
-
-        Instruction : out bit_vector(31 downto 0);
-        Tag : out bit_vector (9 downto 0);
+    InstOut : out bit_vector(31 downto 0);
+        TagOut : out bit_vector (9 downto 0);
         Valid : out bit);
 
 end imem;
@@ -20,16 +21,17 @@ architecture behave of imem is
     signal intAddress : natural;  -- unsigned integer address
     -- Type for memory (only 256+3 bytes implemented)
     --  (extra bytes needed in case of word accesses to last 3 bytes)
-    type memoryType is array (0 to 258) of bit_vector(7 downto 0);
+    type memoryType is array (0 to 15) of bit_vector(42 downto 0);
     -- Memory itself
     signal memory : memoryType;
 
+
+
 begin  -- array
     intAddress <= conv_integer(to_stdlogicvector(ReadAddress(7 downto 0)));
-    Instruction <=
-    memory(intAddress) &
-    memory(intAddress+1) &
-    memory(intAddress+2) &
-    memory(intAddress+3)
+    InstOut <= memory(intAddress)(31 downto 0);
+    TagOut <= memory(intAddress)(41 downto 32);
+    Valid <= memory(intAddress)()
+
     after 5 ns;
 end behave;
